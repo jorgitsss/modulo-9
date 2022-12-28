f998293c2d5b4659cdc3b17028e46643c5c0d7ca
jorgitsss <jorge.godoy.vergara@gmail.com.com>
2022-12-28 00:44:41 -0300

    M9 AE01

---
+++ db/seeds.rb
@@ -0,0 +1,22 @@
+require 'csv'
+puts 'Importing brands 8...'
+CSV.foreach(Rails.root.join('db/seeds/csv/brands.csv'), headers: true) do |row|
+  Brand.create! do |brand|
+    brand.id = row[0]
+    brand.name = row[1]
+    brand.available = row[2]
+  end
+end
+puts '8 brands '
+
+puts 'Importing products 50...'
+a = %w[active inactive discontinued]
+100.times do
+  product = Product.new(
+    name: Faker::Commerce.product_name,
+    description: Faker::Lorem.paragraph,
+    brand_id: rand(1..8),
+    status: a.sample
+  )
+  product.save!
+end
